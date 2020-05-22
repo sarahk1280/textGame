894aeab0d9a13039b7f09063759a90ea3d5459d5
Sarah <sarahk1280@gmail.com>
2020-05-22 15:44:25 +0900

    first commit

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    int[] oddNumbers = { 1, 3, 5, 7, 9 };
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+        Debug.Log(oddNumbers)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
